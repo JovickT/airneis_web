20ec6a9183c33df2a666588a3875b016fa2b9291
JovickT <jovicktchakala@yahoo.com>
2024-06-04 09:52:16 +0200

    [UPD] displaying images from the database

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -30,6 +30,11 @@ const Home = () =>{
 
         // Autres propriétés si nécessaire
     }
+
+    interface Images{
+        images:Array<string>,
+        produit: string
+    }
     
 
     const imgcarrousel = [
@@ -41,8 +46,11 @@ const Home = () =>{
     const [cat, setCat] = useState<Cat[]>([]);
     const [prod, setProd] = useState<Prod[]>([]);
     const [carrousel,setCarrousel] = useState(imgcarrousel);
+    const[imagesProd, setImagesProd] = useState<Images[]>([]);
 
     useEffect(() => {
+        const apiImages = `https://localhost:8000/ip`;
+
         const randomize = (tab: []) =>{
             var i, j, tmp;
             for (i = tab.length - 1; i > 0; i--) {
@@ -73,6 +81,22 @@ const Home = () =>{
                 console.log('data:',data);
             })
             .catch(error => console.error('Erreur lors de la récupération des données depuis le backend :', error));
+            
+            fetch(apiImages)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setImagesProd(data);
+                console.log('Image:', data);
+                // Traiter les données reçues de l'API ici
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     }, []); 
 
     return(
@@ -112,12 +136,25 @@ const Home = () =>{
             
             <h1 className="titre">Les Highlanders du moment</h1>
             <div className="container-img-accueil">
-                {prod && prod.length > 0 && prod.map((p, index) => (
-                    <Link key={index} to={`/produits?categories=${encodeURIComponent(p.categorie)}&produits=${encodeURIComponent(p.nom)}`} className="txt-img-accueil row col-12 col-md-6 col-lg-2">
-                        <img src={canape} alt="" className="img-accueil"/>
-                        <span className="font-bolder">{p.nom}</span>
-                    </Link>
-                ))}
+            {prod.map((p, index) => {
+                        const imageProd = imagesProd.find(img => img.produit === p.nom);
+                        const imageSrc = imageProd ? `${imageProd.images[0]}` : canape;
+
+                        return (
+                            <div key={index} className="col-3 mx-3">
+                                <Link to={`/produits?categories=${encodeURIComponent(p.categorie)}&produits=${encodeURIComponent(p.nom)}`}>
+                                    <img src={imageSrc} alt={p.nom} className="img-produit mb-3"/>
+                                </Link>
+                                <div className="d-flex justify-content-between">
+                                    <p>{p.nom}</p>
+                                    <p>{p.prix}€</p>
+                                </div>
+                                <div className="text-end moving-up">
+                                    {p.quantite <= 10 ? <p>Stock bientôt épuisé</p> : <p>En stock</p>}
+                                </div>
+                            </div>
+                        );
+                    })}
             </div>
 
         </Layout>
