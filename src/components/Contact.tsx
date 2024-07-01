77b2cf9368d63ba52c9e494ae8844ebe0af5d338
emilienbld <emilien.bld03@gmail.com>
2024-07-01 20:33:46 +0200

    css avec responsive : CGU, ML, CONTACT

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,27 +5,27 @@ const Contact = () =>{
 
     return(
         <Layout>
-            <div className="d-flex flex-column align-items-center bg-page">
+            <div className="d-flex flex-column align-items-center">
                 <form action="" method="post" className="">
                     <fieldset>
                         <legend className="d-flex justify-content-center"><img src={logo} alt="Logo" /></legend>
-                    <div style={{flexDirection: 'row', display: 'flex'}}>
-                        <div style={{ marginRight: '10px' }}>
-                            <label htmlFor=""  className="contact-label">Nom</label><br/>
-                            <input type="text" name="nom" id="nom" placeholder="Jones" className="contact-input"/>
+                        <div className="contact-form-row">
+                            <div className="contact-form-item">
+                                <label htmlFor="nom" className="contact-label">Nom</label><br/>
+                                <input type="text" name="nom" id="nom" placeholder="Jones" className="contact-input"/>
+                            </div>
+                            <div className="contact-form-item">
+                                <label htmlFor="prenom" className="contact-label">Prénom</label><br/>
+                                <input type="text" name="prenom" id="prenom" placeholder="John" className="contact-input"/>
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="" className="contact-label">Prénom</label><br/>
-                            <input type="text" className="contact-input" name="prenom" id="prenom" placeholder="John" />
+                        <div className="d-flex flex-column align-items-center" >
+                            <label htmlFor="" className="contact-label" style={{width: '100%'}}>Email</label>
+                            <input type="email" className="contact-input" style={{width: '100%'}} name="email" id="email" placeholder="exemple@xyz.fr"/>
+                            <label htmlFor="" className="contact-label" style={{width: '100%'}}>Message</label>
+                            <textarea className="contact-input" style={{width: '100%'}} name="message" id="message" cols={30} rows={5} placeholder="Message....."/>
+                            <button type="submit" className="contact-button">Envoyer</button>
                         </div>
-                    </div>
-                    <div className="d-flex flex-column align-items-center" >
-                        <label htmlFor="" className="contact-label" style={{width: '100%'}}>Email</label>
-                        <input type="email" className="contact-input" style={{width: '100%'}} name="email" id="email" placeholder="exemple@xyz.fr"/>
-                        <label htmlFor="" className="contact-label" style={{width: '100%'}}>Message</label>
-                        <textarea className="contact-input" style={{width: '100%'}} name="message" id="message" cols={30} rows={5} placeholder="Message....."/>
-                        <button type="submit" className="contact-button">Envoyer</button>
-                    </div>
                     </fieldset>
                 </form>
             </div>
