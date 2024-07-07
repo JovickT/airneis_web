23df8ed2cbb6d3d893700df34044f6d01308396b
emilienbld <emilien.bld03@gmail.com>
2024-07-07 12:55:54 +0200

    CSS et Responsive de la page Categorie

--- src/components/Categorie.tsx
+++ src/components/Categorie.tsx
@@ -77,25 +77,25 @@ const Categorie = () =>{
 
     return(
         <Layout>
-            <div className=" text-center">
-                <span className="font-bolder fs-1 text-light position-absolute z-3 reglage">{catValue}</span>
+            <div className="text-center">
+                <span className="font-bolder fs-1 text-light position-absolute z-4 reglage">{catValue}</span>
                 <img src={cascade} alt="canape" className="mb-5 carrousel-size"/>
                 <p className="font-bolder">DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION</p>
-                <div className="row justify-content-center">
+                <div className="row justify-content-center disposition-categorie">
                 {prod.map((p, index) => {
                         const imageProd = imagesProd.find(img => img.produit === p.nom);
                         const imageSrc = imageProd ? `${imageProd.images[0]}` : canape;
 
                         return (
-                            <div key={index} className="col-3 mx-3">
+                            <div key={index} className="col-3 mx-3 contenu-catgorie">
                                 <Link to={`/produits?categories=${encodeURIComponent(p.categorie.nom)}&produits=${encodeURIComponent(p.nom)}`}>
-                                    <img src={imageSrc} alt={p.nom} className="img-produit mb-3"/>
+                                    <img src={imageSrc} alt={p.nom} className="img-produit-categorie"/>
                                 </Link>
                                 <div className="d-flex justify-content-between">
                                     <p>{p.nom}</p>
                                     <p>{p.prix}€</p>
                                 </div>
-                                <div className="text-end moving-up">
+                                <div className="text-end moving-up test2">
                                     {p.quantite <= 10 ? <p>Stock bientôt épuisé</p> : <p>En stock</p>}
                                 </div>
                             </div>
