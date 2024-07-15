717e3b725ad351328a900343a8a917cc83bb987d
JovickT <jovicktchakala@yahoo.com>
2024-07-15 17:26:46 +0200

    [Fix] products file

--- src/components/Produit.tsx
+++ src/components/Produit.tsx
@@ -3,13 +3,13 @@ import  cascade  from "../img/bannierejpg.jpg";
 import  lit  from "../img/lit.jpg";
 import  armoire  from "../img/armoire.jpg";
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import Modal from "./Modal";
 
 
-const Produit = () => {
+const Produit = () =>{
 
-    interface Prod {
+    interface Prod{
         categorie: {
             id_cat: number,
             nom: string
@@ -17,30 +17,40 @@ const Produit = () => {
         date_creation: string,
         description: string,
         marque: Object,
+        image: string,
         nom: string,
         prix: number,
-        image: string,
         quantite: number,
-        reference: string
+        reference:string
     }
 
-    const [add, setAdd] = useState<{ nom: string; prix: number; description: string; quantite: number; }[]>(() => {
+    //permet d'avoir l'état du panier et le mettre à jour
+    const [add, setAdd] = useState<{ nom: string; prix: number; description: string; quantite: number;}[]>(() => {
+
+        // Récupérer le contenu du panier depuis le localStorage
         const panierString = localStorage.getItem('panier');
+
+        // Si le panier existe, le parser et le retourner. Sinon, retourner un tableau vide.
         return panierString ? JSON.parse(panierString) : [];
+
     });
 
-    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const prodValue = searchParams.get('produits');
     const catValue = searchParams.get('categories');
-    const [theProd, setTheProd] = useState<Prod[]>([]);
-    const [produitSimilaire, setProduitSimilaire] = useState<Prod[]>([]);
+    const [theProd,setTheProd] = useState<Prod[]>([]);
+    const [produitSimilaire,setProduitSimilaire] = useState<Prod[]>([]);
     const [loading, setLoading] = useState(true);
     const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
+
+        // Vérifier si le paramètre `cat` existe et n'est pas vide
         if ((prodValue && prodValue.length > 0) && (catValue && catValue.length > 0)) {
+            // 2. Construire l'URL pour l'API
             const apiUrl = `https://localhost:8000/produits?categories=${encodeURIComponent(catValue)}&produits=${encodeURIComponent(prodValue)}`;
+
+            // 3. Utiliser `fetch` pour envoyer la requête à l'API
             fetch(apiUrl)
                 .then(response => {
                     if (!response.ok) {
@@ -51,11 +61,11 @@ const Produit = () => {
                 .then(data => {
                     setTheProd(data.theProduct);
                     setProduitSimilaire(data.similary);
-                    setLoading(false);
+                    setLoading(false); // Arrête le chargement
                     console.log('Data:', data);
+                    // Traiter les données reçues de l'API ici
                 })
                 .catch(error => {
-                    navigate('/');
                     console.error('There was a problem with the fetch operation:', error);
                 });
         } else {
@@ -64,6 +74,7 @@ const Produit = () => {
         }
     }, [prodValue]);
 
+    //les images lié au carrousel(pour l'instant c'est juste pour avoir six image dans le carrousel)
     const imgcarrousel = [
         'un',
         'deux',
@@ -71,27 +82,28 @@ const Produit = () => {
         'quatre',
         'cinq',
         'six'
-    ];
+    ]
 
+
+    // les éléments qui constitut les informations importante d'uun produit. (il manque juste le lien vers l'image qui correspond au produit)
     const produitPage = theProd.length > 0 ? {
         'nom': theProd[0].nom,
         'categorie': theProd[0].categorie.nom,
         'prix': theProd[0].prix,
         'description': theProd[0].description,
-        'quantite': theProd[0].quantite,
-        'image': theProd[0].image
+        'quantite': theProd[0].quantite
     } : {
         'nom': '',
         'categorie': '',
         'prix': 0,
         'description': '',
-        'quantite': 0,
-        'image': 'image'
+        'quantite': 0
     };
 
-    const [carrousel, setCarrousel] = useState(imgcarrousel);
+    const [carrousel,setCarrousel] = useState(imgcarrousel);
+ 
+    const handleAddStorage = () =>{
 
-    const handleAddStorage = () => {
         const produitExiste = add.find(item => item.nom === produitPage.nom);
         if (produitExiste) {
             const updatedAdd = add.map(item =>
@@ -101,7 +113,7 @@ const Produit = () => {
             );
             setAdd(updatedAdd);
             localStorage.setItem('panier', JSON.stringify(updatedAdd));
-            setShowModal(true);
+            setShowModal(true); // Afficher la modal
         } else {
             const updatedAdd = [...add, { ...produitPage, quantite: 1 }];
             setAdd(updatedAdd);
@@ -113,72 +125,131 @@ const Produit = () => {
         setShowModal(false);
     }
 
-    const affiche = localStorage.getItem('panier');
+    const affiche = localStorage.getItem('panier'); //affiche les éléments du panier en JSON
+
     if (affiche !== null) {
-        const res = JSON.parse(affiche);
+        const res = JSON.parse(affiche); //affiche les éléments du panier en tant que tableau d'objet
+
         console.log('mon panier :', res, '\ntaille du panier :', res.length);
+
     } else {
         console.error('raaaaaaaah');
     }
 
-    return (
+    return(
+        // <Layout>
+        //     <div>
+        //         <img src={cascade} alt="canape" className="carrousel-size mb-5"/>
+        //         <div className="container">
+        //             <Modal show={showModal} handleClose={handleCloseModal} />
+        //             <div className="row">
+        //                 <div id="carouselExampleSlidesOnly" className="carousel slide col-6" data-bs-ride="carousel">
+        //                 <div className="carousel-indicators">
+        //                         {carrousel.map((img,index) =><button key={index}
+        //                         type="button"
+        //                         data-bs-target="#carouselExampleSlidesOnly"
+        //                         data-bs-slide-to={index}
+        //                         className={index === 0 ? "active" : ""}
+        //                         aria-current={index === 0 ? "true" : undefined}
+        //                         aria-label={`Slide ${index}`}
+        //                         ></button>)}
+        //                     </div>
+        //                     <div className="carousel-inner">
+        //                         {carrousel.map((img,index) =><div key={index}
+        //                         className={index === 0 ? "carousel-item active" : "carousel-item"}
+        //                         data-bs-interval="5000">
+        //                             <img src={armoire} className="d-block w-100" alt="..."/>
+        //                         </div>
+        //                         )}
+        //                     </div>
+        //                 </div>
+        //                 <div className="col desc-produits">
+        //                     <div className="d-flex justify-content-between mb-3">
+        //                         <span className="font-bolder">{produitPage.prix}€</span>
+        //                         <div className="d-flex flex-column">
+        //                             <span className="text-uppercase font-bolder">{produitPage.nom}</span>
+        //                             <span className="text-end">{produitPage.quantite > 10? "En stock":"bientôt en rupture de stock"}</span>
+        //                         </div>
+        //                     </div>
+        //                     <div className="description">
+        //                         <p>{produitPage.description}</p>
+        //                     </div>
+        //                     <div className="text-center">
+        //                         <button className="produit-button" onClick={handleAddStorage}>ATOUTER AU PANIER</button>
+        //                     </div>
+        //                 </div>
+        //                 <div className="text-center text-color mt-5">
+        //                     <h1>PRODUIT SIMILAIRE</h1>
+        //                     <div className="row justify-content-center">
+        //                     {produitSimilaire.map((p,index) =><Link key={index} to={`/produits?categories=${encodeURIComponent(produitPage.categorie)}&produits=${encodeURIComponent(p.nom)}`} className="row text-center text-decoration-none col-3 my-3 mx-3">
+        //                             <img src={lit} alt="" className=" mb-2 rounded-5"/>
+        //                             <span className="text-dark font-bolder">{p.nom}</span>
+        //                         </Link>
+        //                     )}
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+               
+        //     </div>
+        // </Layout>
         <Layout>
-            <div>
-                <img src={cascade} alt="canape" className="carrousel-size mb-5" />
-                <div className="container">
-                    <Modal show={showModal} handleClose={handleCloseModal} />
-                    <div className="row">
-                        <div id="carouselExampleSlidesOnly" className="carousel slide col-12 col-md-6" data-bs-ride="carousel">
-                            <div className="carousel-indicators">
-                                {carrousel.map((img, index) => <button key={index}
-                                    type="button"
-                                    data-bs-target="#carouselExampleSlidesOnly"
-                                    data-bs-slide-to={index}
-                                    className={index === 0 ? "active" : ""}
-                                    aria-current={index === 0 ? "true" : undefined}
-                                    aria-label={`Slide ${index}`}
-                                ></button>)}
+        <div>
+            <img src={cascade} alt="canape" className="carrousel-size mb-5" />
+            <div className="container">
+                <Modal show={showModal} handleClose={handleCloseModal} />
+                <div className="row">
+                    <div id="carouselExampleSlidesOnly" className="carousel slide col-12 col-md-6" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            {carrousel.map((img, index) => <button key={index}
+                                type="button"
+                                data-bs-target="#carouselExampleSlidesOnly"
+                                data-bs-slide-to={index}
+                                className={index === 0 ? "active" : ""}
+                                aria-current={index === 0 ? "true" : undefined}
+                                aria-label={`Slide ${index}`}
+                            ></button>)}
+                        </div>
+                        <div className="carousel-inner">
+                            {carrousel.map((img, index) => <div key={index}
+                                className={index === 0 ? "carousel-item active" : "carousel-item"}
+                                data-bs-interval="5000">
+                                <img src={armoire} className="d-block w-100" alt="..." />
                             </div>
-                            <div className="carousel-inner">
-                                {carrousel.map((img, index) => <div key={index}
-                                    className={index === 0 ? "carousel-item active" : "carousel-item"}
-                                    data-bs-interval="5000">
-                                    <img src={armoire} className="d-block w-100" alt="..." />
+                            )}
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 desc-produits">
+                        <div className="product-info">
+                            <div className="d-flex justify-content-between mb-3">
+                                <span className="font-bolder">{produitPage.prix}€</span>
+                                <div className="d-flex flex-column">
+                                    <span className="text-uppercase font-bolder">{produitPage.nom}</span>
+                                    <span className="text-end">{produitPage.quantite > 10 ? "En stock" : "bientôt en rupture de stock"}</span>
                                 </div>
-                                )}
+                            </div>
+                            <div className="description">
+                                <p>{produitPage.description}</p>
                             </div>
                         </div>
-                        <div className="col-12 col-md-6 desc-produits">
-                            <div className="product-info">
-                                <div className="d-flex justify-content-between mb-3">
-                                    <span className="font-bolder">{produitPage.prix}€</span>
-                                    <div className="d-flex flex-column">
-                                        <span className="text-uppercase font-bolder">{produitPage.nom}</span>
-                                        <span className="text-end">{produitPage.quantite > 10 ? "En stock" : "bientôt en rupture de stock"}</span>
-                                    </div>
-                                </div>
-                                <div className="description">
-                                    <p>{produitPage.description}</p>
-                                </div>
-                                <div className="text-center">
-                                    <button onClick={handleAddStorage}>AJOUTER AU PANIER</button>
-                                </div>
-                            </div>
-                            <div className="text-center text-color mt-5">
-                                <h1>PRODUIT SIMILAIRE</h1>
-                                <div className="row justify-content-center">
-                                    {produitSimilaire.map((p, index) => <Link key={index} to={`/produits?categories=${encodeURIComponent(produitPage.categorie)}&produits=${encodeURIComponent(p.nom)}`} className="row text-center text-decoration-none col-3 my-3 mx-3">
-                                        <img src={lit} alt="" className=" mb-2 rounded-5" />
-                                        <span className="text-dark font-bolder">{p.nom}</span>
-                                    </Link>
-                                    )}
-                                </div>
-                            </div>
+                        <div className="text-center">
+                            <button className="produit-button" onClick={handleAddStorage}>AJOUTER AU PANIER</button>
+                        </div>
+                    </div>
+                    <div className="text-center text-color mt-5">
+                        <h1>PRODUIT SIMILAIRE</h1>
+                        <div className="row justify-content-center produits-similaires">
+                            {produitSimilaire.map((p, index) => <Link key={index} to={`/produits?categories=${encodeURIComponent(produitPage.categorie)}&produits=${encodeURIComponent(p.nom)}`} className="row text-center text-decoration-none col-3 my-3 produit-similaire-item">
+                                <img src={p.image} alt="" className="mb-2 rounded-5" />
+                                <span className="text-dark font-bolder">{p.nom}</span>
+                            </Link>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-        </Layout>
+        </div>
+    </Layout>
     )
 }
 
