a4fcd9a0eb7272afe5dcf56288944b5c986484e9
JovickT <jovicktchakala@yahoo.com>
2024-08-07 08:59:00 +0200

    [ADD] Protection route

--- src/components/CheckoutPayement.tsx
+++ src/components/CheckoutPayement.tsx
@@ -16,17 +16,40 @@ interface Payment {
   };
 }
 
+interface PaymentBis {
+  id: string;
+  brand: string;
+  last4: string;
+  exp_month: string;
+  exp_year: string;
+}
 
 interface Panier {
   email: string | null;
 }
 
 const CheckoutForm = () => {
+
+  type CardBrand = 'visa' | 'mastercard' | 'amex' | 'discover' | 'jcb' | 'diners' | 'unionpay' | 'default';
+
+  const cardBrandIcons: Record<CardBrand, string> = {
+      visa: 'fab fa-cc-visa',
+      mastercard: 'fab fa-cc-mastercard',
+      amex: 'fab fa-cc-amex',
+      discover: 'fab fa-cc-discover',
+      jcb: 'fab fa-cc-jcb',
+      diners: 'fab fa-cc-diners-club',
+      unionpay: 'far fa-credit-card',
+      default: 'far fa-credit-card', // Icône par défaut pour les autres marques
+  };
+
   const [error, setError] = useState<string>();
   const [processing, setProcessing] = useState(false);
   const [succeeded, setSucceeded] = useState(false);
   const [saveCard, setSaveCard] = useState(false);
-  const [savedPaymentMethods, setSavedPaymentMethods] = useState<Payment[]>([]);
+  const [savedPaymentMethods, setSavedPaymentMethods] = useState<PaymentBis[]>([]);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<string | null>(null);
+  const [useSavedMethod, setUseSavedMethod] = useState(false); // Gestion de la méthode sauvegardée
 
   const stripe = useStripe();
   const elements = useElements();
@@ -52,130 +75,202 @@ const CheckoutForm = () => {
       email: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!).email : null
   });
 
-  // useEffect(() => {
-  //   const fetchPaymentMethods = async () => {
-  //     const response = await fetch('https://localhost:8000/api/payment-methods', {
-  //       method: 'GET',
-  //       credentials: 'include',
-  //     });
-  //     const data = await response.json();
-  //     setSavedPaymentMethods(data.paymentMethods);
-  //   };
+  useEffect(() => {
+    // Récupérer les méthodes de paiement sauvegardées
+    const fetchSavedPaymentMethods = async () => {
+      try {
+        const response = await axios.post('/get-payment-methods', userStorage);
 
-  //   fetchPaymentMethods();
-  // }, []);
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = response.data;
+        setSavedPaymentMethods(data.paymentMethods);
+        
+      } catch (error) {
+        console.error('Error fetching saved payment methods:', error);
+      }
+    };
+
+    fetchSavedPaymentMethods();
+  }, [userStorage]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setProcessing(true);
-    
+  
     if (!stripe || !elements) {
       setError('Stripe.js has not loaded yet.');
       setProcessing(false);
       return;
     }
-
+  
     setIsLoading(true);
-
+  
     const clientSecret = localStorage.getItem('clientSecret');
     if (!clientSecret) {
       setError('Client secret not found.');
       setProcessing(false);
       return;
     }
+  
+    if (useSavedMethod && selectedPaymentMethod) {
+      // Utilisation de la méthode de paiement sauvegardée
+      
+      try {
+        const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: selectedPaymentMethod,
+        });
 
-    const cardElement = elements.getElement(CardElement);
-    if (!cardElement) {
-      setError('Card element not found.');
-      setProcessing(false);
-      return;
-    }
+        console.log('je suis au bonn endroit:',paymentIntent);
 
-    try {
-      const { error: createPaymentMethodError, paymentMethod } = await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-      });
-    
-      if (createPaymentMethodError) {
-        setError(createPaymentMethodError.message);
-        console.log('Error creating payment method:', createPaymentMethodError);
-        return;
-      }
-    
-      const { id: paymentMethodId, card } = paymentMethod;
-      console.log('Payment method created:', paymentMethodId);
-    
-      const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: paymentMethodId,
-      });
-    
-      if (error) {
-        setError(`Payment failed: ${error.message}`);
-        console.log('Error confirming card payment:', error);
-        setProcessing(false);
-        setMessage('Echec de Paiement!');
-        return;
-      }
-    
-      if (paymentIntent && paymentIntent.status === 'succeeded') {
-        console.log('Payment succeeded:', paymentIntent);
-    
-        // Stockez uniquement les informations nécessaires
-        const cardInfo = {
-          brand: card?.brand,
-          last4: card?.last4,
-          exp_month: card?.exp_month,
-          exp_year: card?.exp_year,
-        };
-    
-        try {
-          localStorage.setItem('payment_method', JSON.stringify(cardInfo));
-          const response = await fetch('https://localhost:8000/api/commande', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(commande),
-            credentials: 'include',
-          });
-    
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
+        if (error) {
+          setError(`Payment failed: ${error.message}`);
+          setProcessing(false);
+          setMessage('Echec de Paiement!');
+          return;
+        }
+  
+        if (paymentIntent && paymentIntent.status === 'succeeded') {
+          const paymentInfo = {
+            id: selectedPaymentMethod,
+            // On pourrait obtenir plus d'informations à partir de la méthode de paiement si nécessaire
+          };
+  
+          // Mettre à jour les informations de la commande avec la méthode de paiement sauvegardée
+          setCommande((prevCommande) => ({
+            ...prevCommande,
+            paiement: paymentInfo,
+          }));
+  
+          await new Promise((resolve) => setTimeout(resolve, 0));
+
+          const response = await axios.post('/commande', JSON.stringify({
+            ...commande,
+            paiement: paymentInfo,
+          }));
+
+          if (response.status < 200 || response.status >= 300) {
+              throw new Error('Network response was not ok');
           }
-    
+
+  
           setMessage('Paiement réussi!');
-          const data = await response.json();
-          console.log('Commande response data:', data);
+          const data = response.data;
           localStorage.setItem('orderData', JSON.stringify(data));
           localStorage.removeItem('clientSecret');
           localStorage.removeItem('panier');
           localStorage.removeItem('TTC');
           localStorage.removeItem('info-livraison');
-    
+  
+          navigate('/checkoutFini');
+        } else {
+          setError(`Payment failed: ${paymentIntent?.status}`);
+          setProcessing(false);
+          setMessage('Echec de Paiement!');
+        }
+      } catch (error) {
+        console.error('Error confirming card payment:', error);
+        setError('An error occurred while processing your payment. Please try again.');
+        setProcessing(false);
+      }
+    } else {
+      // Utilisation d'une nouvelle carte
+      const cardElement = elements.getElement(CardElement);
+      if (!cardElement) {
+        setError('Card element not found.');
+        setProcessing(false);
+        return;
+      }
+  
+      try {
+        const { error: createPaymentMethodError, paymentMethod } = await stripe.createPaymentMethod({
+          type: 'card',
+          card: cardElement,
+        });
+  
+        if (createPaymentMethodError) {
+          setError(createPaymentMethodError.message);
+          setProcessing(false);
+          return;
+        }
+  
+        const { id: paymentMethodId, card } = paymentMethod;
+  
+        const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: paymentMethodId,
+        });
+  
+        if (error) {
+          setError(`Payment failed: ${error.message}`);
+          setProcessing(false);
+          setMessage('Echec de Paiement!');
+          return;
+        }
+  
+        if (paymentIntent && paymentIntent.status === 'succeeded') {
+          const cardInfo = {
+            id: paymentMethod?.id,
+            brand: card?.brand,
+            last4: card?.last4,
+            exp_month: card?.exp_month,
+            exp_year: card?.exp_year,
+          };
+  
+          localStorage.setItem('payment_method', JSON.stringify(cardInfo));
+          setCommande((prevCommande) => ({
+            ...prevCommande,
+            paiement: cardInfo,
+          }));
+  
+          await new Promise((resolve) => setTimeout(resolve, 0));
+  
+          const response = await fetch('https://localhost:8000/api/commande', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              ...commande,
+              paiement: cardInfo,
+            }),
+            credentials: 'include',
+          });
+  
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+  
+          setMessage('Paiement réussi!');
+          const data = await response.json();
+          localStorage.setItem('orderData', JSON.stringify(data));
+          localStorage.removeItem('clientSecret');
+          localStorage.removeItem('panier');
+          localStorage.removeItem('TTC');
+          localStorage.removeItem('info-livraison');
+  
           if (saveCard) {
             const paymentMethodResponse = await stripe.createPaymentMethod({
               type: 'card',
               card: cardElement,
             });
-    
+  
             if (paymentMethodResponse.error) {
               setError(paymentMethodResponse.error.message);
-              console.log('Error creating payment method for saving:', paymentMethodResponse.error);
             } else {
               try {
-                console.log('Attaching payment method...');
-                const response = await axios.post('https://localhost:8000/api/attach-payment-method', {
+                const response = await axios.post('/attach-payment-method', {
                   user: {
-                    email: userStorage?.email,
+                    email: userStorage.email,
                   },
                   paymentMethodId: paymentMethodResponse.paymentMethod.id,
                 });
-    
+  
                 if (response.status < 200 || response.status >= 300) {
                   throw new Error('Network response was not ok');
                 }
-    
+  
                 const data = response.data;
                 console.log('Attach payment method response data:', data);
               } catch (error) {
@@ -184,25 +279,21 @@ const CheckoutForm = () => {
               }
             }
           }
-    
+  
           navigate('/checkoutFini');
-        } catch (error) {
-          console.error('Erreur lors de la création du PaymentIntent:', error);
-          setError('Une erreur est survenue lors du traitement de votre commande. Veuillez réessayer.');
+        } else {
+          setError(`Payment failed: ${paymentIntent?.status}`);
+          setProcessing(false);
+          setMessage('Echec de Paiement!');
         }
-      } else {
-        setError(`Payment failed: ${paymentIntent?.status}`);
+      } catch (error) {
+        console.error('Error confirming card payment:', error);
+        setError('An error occurred while processing your payment. Please try again.');
         setProcessing(false);
-        setMessage('Echec de Paiement!');
       }
-    } catch (error) {
-      console.error('Error confirming card payment:', error);
-      setError('An error occurred while processing your payment. Please try again.');
-      setProcessing(false);
     }
-    
-    
   };
+  
 
   const cardStyle = {
     style: {
@@ -224,34 +315,55 @@ const CheckoutForm = () => {
 
   return (
     <form id="payment-form" onSubmit={handleSubmit} className="payment-form">
+    {!useSavedMethod && (
       <div className="form-group">
         <label className="mb-2" htmlFor="card-element">Credit or debit card</label>
         <CardElement id="card-element" options={cardStyle} />
       </div>
-      <button className="checkout-button" id="submit">
-        <span id="button-text">
-          Payer
-        </span>
-      </button>
-      {message && <div id="payment-message" className="alert alert-info">{message}</div>}
-      <div className="mt-3">
-        <label htmlFor="savePayement_card">
-          <input type="checkbox" className="me-2" name="savePayement_card" id="savePayement_card" onChange={(e) => setSaveCard(e.target.checked)} />
-          Enregistrer ce moyen de paiement
-        </label>
-      </div>
-      {savedPaymentMethods.length > 0 && (
-        <div className="saved-payment-methods">
-          <h3>Utiliser un moyen de paiement sauvegardé :</h3>
-          {savedPaymentMethods.map((method) => (
-            <div key={method.id}>
-              <input type="radio" id={method.id} name="saved_payment_method" value={method.id} />
-              <label htmlFor={method.id}>{method.card.brand} **** **** **** {method.card.last4}</label>
+    )}
+    <button className="checkout-button mt-5" id="submit" type="submit" disabled={processing || !stripe || !elements}>
+      <span id="button-text">
+        {useSavedMethod ? 'Payer avec méthode sauvegardée' : 'Payer'}
+      </span>
+    </button>
+    {message && <div id="payment-message" className="alert alert-info mt-2">{message}</div>}
+    <div className="mt-3">
+      <label htmlFor="savePayement_card">
+        <input type="checkbox" className="me-2" name="savePayement_card" id="savePayement_card" onChange={(e) => setSaveCard(e.target.checked)} />
+        Enregistrer ce moyen de paiement
+      </label>
+    </div>
+    {savedPaymentMethods.length > 0 && (
+      <div className="saved-payment-methods mt-5">
+        <h3>Utiliser un moyen de paiement sauvegardé :</h3>
+        {savedPaymentMethods.map((method) => {
+          const iconClass = cardBrandIcons[method.brand as CardBrand] || cardBrandIcons.default;
+          return (
+            <div key={method.id} className="d-flex mt-4">
+              <input 
+                type="checkbox" 
+                id={method.id} 
+                className="me-2" 
+                name="saved_payment_method" 
+                checked={selectedPaymentMethod === method.id}
+                onChange={() => {
+                  if (selectedPaymentMethod === method.id) {
+                    setSelectedPaymentMethod(null);
+                    setUseSavedMethod(false); // Désactiver l'utilisation de la méthode sauvegardée
+                  } else {
+                    setSelectedPaymentMethod(method.id);
+                    setUseSavedMethod(true); // Activer l'utilisation de la méthode sauvegardée
+                  }
+                }} 
+              />
+              <i className={`${iconClass} fa-2x me-2`}></i>
+              <label htmlFor={method.id}>**** **** **** {method.last4} {method.exp_month}/{method.exp_year}</label>
             </div>
-          ))}
-        </div>
-      )}
-    </form>
+          );
+        })}
+      </div>
+    )}
+  </form>
   );
 };
 
